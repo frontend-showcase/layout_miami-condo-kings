897d6107b4aa515844fe37866c417d0ad0c9093d
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-08-19 21:08:45 +0200

    Updates

---
+++ layout_miami-condo-kings.f1ea2672.js
@@ -0,0 +1,2 @@
+"use strict";document.getElementById("form").addEventListener("submit",function(e){e.preventDefault(),document.getElementById("name").value="",document.getElementById("email").value="",document.getElementById("message").value=""});
+//# sourceMappingURL=layout_miami-condo-kings.f1ea2672.js.map
